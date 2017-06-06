b72fbe2b43e866354414bc28ea4e4a968d2bf836
Admin <admin@Admins-Mac.local>
2017-06-06 07:56:36 +0530

    third controller added

---
+++ Table/ThirdViewController.h
@@ -0,0 +1,13 @@
+//
+//  ThirdViewController.h
+//  Table
+//
+//  Created by Admin on 06/06/17.
+//  Copyright © 2017 fis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ThirdViewController : UIViewController
+
+@end
---
+++ Table/ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  Table
+//
+//  Created by Admin on 06/06/17.
+//  Copyright © 2017 fis. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"yeah ! this is third one");
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
